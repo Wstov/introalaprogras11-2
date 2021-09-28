b46a80e1dd94b5140fd70b54e1f41a2e69d920c5
LariB <nlblanco7701@gmail.com>
2021-09-28 17:25:25 -0600

    Semana 01

---
+++ Semana01/src/Libraries/Main.java
@@ -0,0 +1,57 @@
+package Libraries;
+
+//importar libreria
+import org.w3c.dom.xpath.XPathResult;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //PRÁCTICA 01
+
+    //String nombre = "Larissa Blanco";
+    //Integer edad = 20;
+    //String direccion = "San Rafael de Heredia";
+
+    //System.out.println(nombre +" "+ edad +" "+ direccion);
+
+    //PRÁCTICA 02
+     //String nombre2 = JOptionPane.showInputDialog("Ingrese su nombre");
+
+     //int edad2 = Integer.parseInt( JOptionPane.showInputDialog("Ingrese su edad"));
+
+     //String direccion1 = JOptionPane.showInputDialog("Ingrese su direccion");
+
+     //double sueldo = Double.parseDouble(JOptionPane.showInputDialog("Ingrese su sueldo"));
+     //JOptionPane.showMessageDialog(null, nombre2 +" "+ edad2 +" "+ direccion1 +" "+ sueldo);
+
+
+    //PRÁCTICA 03
+    //int val1 = 10;
+    //int val2 = 10;
+    //int val3 = 10;
+    //int val4 = 10;
+
+    //int suma =  (val1 + val2 + val3 + val3)/4;
+    //System.out.println(suma);
+
+
+    //PRÁCTICA 04
+    //int num1 = Integer.parseInt( JOptionPane.showInputDialog("Ingrese el número 1"));
+    //int num2 = Integer.parseInt( JOptionPane.showInputDialog("Ingrese el número 2"));
+    //int num3 = Integer.parseInt( JOptionPane.showInputDialog("Ingrese el número 3"));
+    //int num4 = Integer.parseInt( JOptionPane.showInputDialog("Ingrese el número 4"));
+    //int suma1 =  num1 + num2 + num3 + num4;
+
+    //JOptionPane.showMessageDialog(null, suma1);
+
+     //PRÁCTICA 05
+     String nom = JOptionPane.showInputDialog("Ingrese su nombre");
+     JOptionPane.showMessageDialog(null, "Hola" +" "+ nom + ", "+ "¿Cómo estás?");
+    }
+}
